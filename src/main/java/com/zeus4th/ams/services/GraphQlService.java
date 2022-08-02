8738b35e60b9e5f50c3298d7ff5c47a84c08ef6b
Aabhishek29 <starkabhishek29@gmail.com>
2022-08-02 14:02:26 +0530

    Graphql Service and API created

---
+++ src/main/java/com/zeus4th/ams/services/GraphQlService.java
@@ -0,0 +1,57 @@
+package com.zeus4th.ams.services;
+
+import java.io.File;
+import java.io.IOException;
+import javax.annotation.PostConstruct;
+
+import com.zeus4th.ams.services.datafetcher.UserDataFetcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+@Service
+public class GraphQlService {
+
+  @Autowired
+  private UserDataFetcher userDataFetcher;
+
+
+  @Value("classpath:graphql/users.graphql")
+  Resource resource;
+  private GraphQL graphQL;
+
+
+  @PostConstruct
+  private void loadSchema() throws IOException {
+
+    File file = resource.getFile();
+
+    // Get the graphql file
+    TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser().parse(file);
+    RuntimeWiring runtimeWiring = buildRuntimeWiring();
+    GraphQLSchema graphQLSchema
+        = new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+    graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+  }
+
+  private RuntimeWiring buildRuntimeWiring() {
+    return RuntimeWiring.newRuntimeWiring()
+        .type("Query",
+            typeWiring -> typeWiring
+                .dataFetcher("allUsers", userDataFetcher))
+        .build();
+  }
+
+  public ExecutionResult executeGraphQL(String query) {
+    return graphQL.execute(query);
+  }
+
+}

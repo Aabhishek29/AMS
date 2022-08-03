b3c42d5a544afe6f71baac84c4fcac6f27cd1df8
Abhi-sirohi <Abhinaysirohi_19151153.bca@geu.ac.in>
2022-08-03 13:55:27 +0530

    .

--- src/main/java/com/zeus4th/ams/controlller/UserController.java
+++ src/main/java/com/zeus4th/ams/controlller/UserController.java
@@ -10,19 +10,13 @@ import com.zeus4th.ams.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
-//@CrossOrigin(origins = {"http://localhost:8080","https://portal-ams.herokuapp.com/"})
+@CrossOrigin(origins = {"http://localhost:8080","https://portal-ams.herokuapp.com/","http://localhost:8080"},
+        allowedHeaders ={"Authorization", "Cache-Control", "Content-Type"},allowCredentials = "true",methods = {
+        RequestMethod.POST,RequestMethod.GET,RequestMethod.DELETE,RequestMethod.PUT,RequestMethod.OPTIONS
+})
 @RestController
 @RequestMapping("/api")
 public class UserController {

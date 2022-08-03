7d4d6bddb58d53147e77ed58cd5c5c0fdf1e145c
Abhi-sirohi <Abhinaysirohi_19151153.bca@geu.ac.in>
2022-08-03 13:05:02 +0530

    Changed the cors config setting

--- src/main/java/com/zeus4th/ams/AmsApplication.java
+++ src/main/java/com/zeus4th/ams/AmsApplication.java
@@ -2,7 +2,15 @@ package com.zeus4th.ams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
@@ -12,5 +20,37 @@ public class AmsApplication {
         System.out.println("Started AMS");
         SpringApplication.run(AmsApplication.class, args);
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedMethods(CorsConfiguration.ALL)
+//                        .allowedHeaders(CorsConfiguration.ALL)
+//                        .allowedOriginPatterns(CorsConfiguration.ALL).allowedOrigins("*").maxAge(3600);
+//            }
+//        };
+//    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "https://portal-ams.herokuapp.com","http" +
+                "://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+
+
 
 }
